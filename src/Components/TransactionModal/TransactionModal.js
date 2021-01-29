885df4c9ae24d1b93c83a8b2bd3174f455a26076
mattcbodily <mattcbodily@gmail.com>
2021-01-29 16:06:30 -0700

    added preliminary styling to TransactionModal

--- src/Components/TransactionModal/TransactionModal.js
+++ src/Components/TransactionModal/TransactionModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import './TransactionModal.css'
 
 const TransactionModal = props => {
     const [category, setCategory] = useState('income')
@@ -20,20 +21,22 @@ const TransactionModal = props => {
     }
 
     return (
-        <section>
-            <h4>Add a transaction</h4>
-            <form>
-                <select value={category} onChange={e => setCategory(e.target.value)}>
-                    <option value='income'>Income</option>
-                    <option value='expense'>Expense</option>
-                </select>
-                <input value={description} onChange={e => setDescription(e.target.value)} />
-                <input type='number' value={amount} onChange={e => setAmount(e.target.value)} />
-                <input type='date' value={transactionDate} onChange={e => setTransactionDate(e.target.value)} />
-                <button onClick={e => addTransaction(e)}>Add</button>
-                <button onClick={() => props.setModalView(false)}>Cancel</button>
-            </form>
-        </section>
+        <div className='modal-backdrop'>
+            <section className='transaction-modal'>
+                <h4>Add a transaction</h4>
+                <form>
+                    <select value={category} onChange={e => setCategory(e.target.value)}>
+                        <option value='income'>Income</option>
+                        <option value='expense'>Expense</option>
+                    </select>
+                    <input value={description} onChange={e => setDescription(e.target.value)} />
+                    <input type='number' value={amount} onChange={e => setAmount(e.target.value)} />
+                    <input type='date' value={transactionDate} onChange={e => setTransactionDate(e.target.value)} />
+                    <button onClick={e => addTransaction(e)}>Add</button>
+                    <button onClick={() => props.setModalView(false)}>Cancel</button>
+                </form>
+            </section>
+        </div>
     )
 }
 
