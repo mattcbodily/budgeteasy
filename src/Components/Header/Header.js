e531c634ea63a4a7909bb8cf9eb65883a630a80d
mattcbodily <mattcbodily@gmail.com>
2021-01-29 15:40:57 -0700

    dropdown functionality added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
+import menuIcon from '../../assets/icons/menu.svg'
+import closeIcon from '../../assets/icons/x.svg'
 import './Header.css'
 
 const Header = props => {
+    const [dropdown, setDropdown] = useState('closed')
+
+    const closeMenu = () => {
+        setDropdown('close-menu');
+        setTimeout(() => {
+            setDropdown('closed');
+        }, 225);
+    }
+
     return (
         <header className='main-header'>
             {props.location.pathname !== '/'
               ? (
                   <section className='header-flex'>
                     <h2>Welcome, {props.firstName}</h2>
-                    <nav>
-                        <Link to='/dashboard'>Home</Link>
-                        <Link to='/profile'>Profile</Link>
+                    <div className='menu-bubble' onClick={() => setDropdown('open-menu')}>
+                        <img src={menuIcon} alt='Main menu' />
+                    </div>
+                    <nav className={`dropdown-menu ${dropdown}`}>
+                        <img src={closeIcon} alt='Close menu' onClick={closeMenu}/>
+                        <Link to='/dashboard' className='nav-links' onClick={() => setDropdown('closed')}>Home</Link>
+                        <Link to='/profile' className='nav-links' onClick={() => setDropdown('closed')}>Profile</Link>
                     </nav>
                   </section>
               )
